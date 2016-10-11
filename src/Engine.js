9cf42016c4ae687d259f6bf550568fcd7877b71a
pecqueuxnathan <pecqueuxnathan@gmail.com>
2016-10-11 11:31:24 +0200

    First scenario unit test ACCEPT =D

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,64 @@
-'use strict';
+"use strict";
 
 var Engine = function () {
 
 // private attributes and methods
 
 // public methods
+
+    this.initPaletto = function() {
+        var paletto =
+            [
+                ["black", "green", "white", "blue", "red","white" ],
+                ["yellow", "white", "green", "red", "yellow", "blue"],
+                ["blue", "yellow", "blue", "white", "black", "red"],
+                ["red", "black", "red", "green", "blue", "white"],
+                ["white", "green", "yellow", "black", "yellow", "green"],
+                ["yellow", "blue","black", "red", "green", "black"]
+                ];
+        return paletto;
+    }
+
+
+
+
+
+
+
+    /*
+        DO NOT TOUCH THIS FUCKING CODE BUDDY !!! Thanks =)
+     */
+
+
+
+
+    // Warn if overriding existing method
+    if(Array.prototype.equals)
+        console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+    Array.prototype.equals = function (array) {
+        // if the other array is a falsy value, return
+        if (!array)
+            return false;
+
+        // compare lengths - can save a lot of time
+        if (this.length != array.length)
+            return false;
+
+        for (var i = 0, l=this.length; i < l; i++) {
+            // Check if we have nested arrays
+            if (this[i] instanceof Array && array[i] instanceof Array) {
+                // recurse into the nested arrays
+                if (!this[i].equals(array[i]))
+                    return false;
+            }
+            else if (this[i] != array[i]) {
+                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+                return false;
+            }
+        }
+        return true;
+    }
+// Hide method from for-in loops
+    Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
 
 var PalettoTestCase = TestCase("PalettoTestCase");
+
+var engine = new Engine();
 
 PalettoTestCase.prototype.testStory1 = function () {
     /*  var paletto =
      [["black", "green", "white", "blue", "red","white" ]]
-     [["yellow", "white", "green", "red", "yellow", "bleu"]]
+     [["yellow", "white", "green", "red", "yellow", "blue"]]
      [["blue", "yellow", "blue", "white", "black", "red"]]
      [["red", "black", "red", "green", "blue", "white"]]
      [["white", "green", "yellow", "black", "yellow", "green"]]
@@ -13,12 +15,15 @@ PalettoTestCase.prototype.testStory1 = function () {
 
      */
 
-    var paletto = initPaletto();
-    assertTrue(paletto[0] == ["black", "green", "white", "blue", "red", "white"]);
-    assertTrue(paletto[1] == ["yellow", "white", "green", "red", "yellow", "bleu"]);
-    assertTrue(paletto[2] == ["blue", "yellow", "blue", "white", "black", "red"]);
-    assertTrue(paletto[3] == ["red", "black", "red", "green", "blue", "white"]);
-    assertTrue(paletto[4] == ["white", "green", "yellow", "black", "yellow", "green"]);
-    assertTrue(paletto[5] == ["yellow", "blue","black", "red", "green", "black"]);
+    var paletto = engine.initPaletto();
+
+    assertTrue(paletto[0][0] == "black");
+
+    assertTrue(paletto[0].equals(["black", "green", "white", "blue", "red", "white"]));
+    assertTrue(paletto[1].equals(["yellow", "white", "green", "red", "yellow", "blue"]));
+    assertTrue(paletto[2].equals(["blue", "yellow", "blue", "white", "black", "red"]));
+    assertTrue(paletto[3].equals(["red", "black", "red", "green", "blue", "white"]));
+    assertTrue(paletto[4].equals(["white", "green", "yellow", "black", "yellow", "green"]));
+    assertTrue(paletto[5].equals(["yellow", "blue","black", "red", "green", "black"]));
 
 };
